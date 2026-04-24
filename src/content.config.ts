8a95291c044230035b132f2f1fa4090cef81a85f
Tom Kooij <74959713+TommyKooij@users.noreply.github.com>
2026-04-24 16:02:23 +0200

    24/04/2026

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from 'astro/zod'
+
+const books = defineCollection({
+  loader: glob({ base: 'src/contents/books', pattern: '*.md' }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    rating: z.number(),
+    summary: z.string(),
+  })
+})
+
+export const collections = { books }
